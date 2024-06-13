aa3400db82950fd75da3acc6671d9b5a84c114d3
miguelangel <cifuentesmiguelangel97@gmail.com>
2024-06-13 12:57:48 -0500

    Proyecto final DYP

---
+++ src/credenciales.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAzxHUBXE0s8NEa7SPgyopULkmwZrrkSbM",
+  authDomain: "crud-y-loggin-dyp-parcial-2.firebaseapp.com",
+  projectId: "crud-y-loggin-dyp-parcial-2",
+  storageBucket: "crud-y-loggin-dyp-parcial-2.appspot.com",
+  messagingSenderId: "365899102025",
+  appId: "1:365899102025:web:08e309b8a92c3e7da8cc98"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
